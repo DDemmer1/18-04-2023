f90a790a690d744f30e99a028440646b4ca04c01
Dennis Demmer <Dennis.Demmer@web.de>
2023-04-18 10:41:43 +0200

    Update Application.java

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -4,6 +4,26 @@ public class Application {
 	
 	public static void main(String[] args) {
 		
+			InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(inputStreamReader);
+		try {
+			String input = br.readLine();
+			System.out.println(input);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		
+//-------------------------------------------------
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		
+		System.exit(0);
+		
 		
 		
 	}
